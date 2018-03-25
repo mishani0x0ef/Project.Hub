90a5993b37104e112231f97bce41505adb4e8376
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-03-25 21:39:26 +0300

    Create simple page for versions display.
    Create page to display components versions.

---
+++ src/Project.Hub/Controllers/VersionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Hub.Controllers
+{
+    public class VersionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
