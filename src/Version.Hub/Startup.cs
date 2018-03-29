31eac1c488e59a589ceefbf5049bb38eb843fe76
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-03-30 00:02:40 +0300

    Implement base API for versions resolve.

---
+++ src/Version.Hub/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Version.Hub.Filters;
+using Version.Hub.Config;
+using Version.Hub.Config.Providers;
+
+namespace Version.Hub
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureDependencyInjection(services);
+            services.AddMvc(config => 
+            {
+                config.Filters.Add<NotFoundErrorFillter>();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+
+        private void ConfigureDependencyInjection(IServiceCollection services)
+        {
+            services
+                .AddMemoryCache()
+                .AddSingleton<IVersionProvider, StaticVersionProvider>()
+                ;
+        }
+    }
+}
