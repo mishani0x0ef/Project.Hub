aabe98b58e4b7cc2860c3b1000af2a4d46b9e7b6
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-05-01 20:54:11 +0300

    Implement very base component page.

---
+++ src/Project.Hub/Controllers/ComponentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Hub.Config.Interfaces;
+using Project.Hub.Config.Util;
+using System.Threading.Tasks;
+
+namespace Project.Hub.Controllers
+{
+    public class ComponentController : Controller
+    {
+        private readonly IConfigurationProvider _provider;
+
+        public ComponentController(IConfigurationProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public async Task<IActionResult> Index(string id)
+        {
+            var config = await _provider.GetConfig();
+            foreach (var env in config.Environments)
+            {
+                var components = env.GetAllComponents();
+                foreach(var component in components)
+                {
+                    if(component.Name == id)
+                    {
+                        return View(component);
+                    }
+                }
+            }
+
+            return View(null);
+        }
+    }
+}
