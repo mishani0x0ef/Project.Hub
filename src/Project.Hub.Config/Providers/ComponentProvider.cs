37b852856ac05ce22b3954a99cca616421eb635b
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-05-01 21:41:56 +0300

    Move component details resolution from controller to separate service.

---
+++ src/Project.Hub.Config/Providers/ComponentProvider.cs
@@ -0,0 +1,58 @@
+﻿using Project.Hub.Config.Interfaces;
+using Project.Hub.Config.Entities.ComponentData;
+using System.Threading.Tasks;
+using Project.Hub.Config.Util;
+using System.Collections.Generic;
+
+namespace Project.Hub.Config.Providers
+{
+    public class ComponentProvider : IComponentProvider
+    {
+        private readonly IConfigurationProvider _configProvider;
+
+        public ComponentProvider(IConfigurationProvider configProvider)
+        {
+            _configProvider = configProvider;
+        }
+
+        public async Task<ComponentDetails> GetByName(string name)
+        {
+            var config = await _configProvider.GetConfig();
+            foreach (var env in config.Environments)
+            {
+                var components = env.GetAllComponents();
+                foreach (var component in components)
+                {
+                    if (component.Name == name)
+                    {
+                        var details = new ComponentDetails()
+                        {
+                            Name = component.Name,
+                            Description = component.Description,
+                            Environments = new HashSet<EnvironmentDetails>
+                            {
+                                new EnvironmentDetails()
+                                {
+                                    Name = "Test",
+                                    Description = "Testing Environment",
+                                    Version = "3.0.0",
+                                    Link = ""
+                                },
+                                new EnvironmentDetails()
+                                {
+                                    Name = "Production",
+                                    Description = "Production Environment",
+                                    Version = "2.0.0",
+                                    Link = ""
+                                }
+                            }
+                        };
+                        return details;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- src/Project.Hub/Controllers/ComponentController.cs
+++ src/Project.Hub/Controllers/ComponentController.cs
@@ -1,59 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Project.Hub.Config.Entities.ComponentData;
 using Project.Hub.Config.Interfaces;
-using Project.Hub.Config.Util;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Project.Hub.Controllers
 {
     public class ComponentController : Controller
     {
-        private readonly IConfigurationProvider _provider;
+        private readonly IComponentProvider _provider;
 
-        public ComponentController(IConfigurationProvider provider)
+        public ComponentController(IComponentProvider provider)
         {
             _provider = provider;
         }
 
         public async Task<IActionResult> Index(string id)
         {
-            var config = await _provider.GetConfig();
-            foreach (var env in config.Environments)
-            {
-                var components = env.GetAllComponents();
-                foreach(var component in components)
-                {
-                    if(component.Name == id)
-                    {
-                        var details = new ComponentDetails()
-                        {
-                            Name = component.Name,
-                            Description = component.Description,
-                            Environments = new HashSet<EnvironmentDetails>
-                            {
-                                new EnvironmentDetails()
-                                {
-                                    Name = "Test",
-                                    Description = "Testing Environment",
-                                    Version = "3.0.0",
-                                    Link = ""
-                                },
-                                new EnvironmentDetails()
-                                {
-                                    Name = "Production",
-                                    Description = "Production Environment",
-                                    Version = "2.0.0",
-                                    Link = ""
-                                }
-                            }
-                        };
-                        return View(details);
-                    }
-                }
-            }
-
-            return View(null);
+            var component = await _provider.GetByName(id);
+            return View(component);
         }
     }
 }
