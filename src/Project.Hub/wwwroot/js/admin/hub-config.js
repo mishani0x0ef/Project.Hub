4d0b196fe55bbe6b5a8d634dbabf3805dfb892db
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-05-12 22:24:44 +0300

    Move admin styles and scripts to separate files.

---
+++ src/Project.Hub/wwwroot/js/admin/hub-config.js
@@ -0,0 +1,34 @@
+﻿class HubConfigEditor {
+    constructor(editorId) {
+        this.editor = ace.edit(editorId);
+        this._configureEditor(this.editor);
+        this._enableFullScreenSupport();
+    }
+
+    _configureEditor(editor) {
+        editor.setTheme("ace/theme/monokai");
+        editor.session.setMode("ace/mode/json");
+
+        editor.setOptions({
+            showPrintMargin: false,
+        });
+    }
+
+    _enableFullScreenSupport() {
+        const expandButtons = document.querySelectorAll(".editor-container .btn-expand");
+        expandButtons.forEach(button => button.addEventListener("click", (e) => this._onExpandClicked(e)));
+
+        const collapseButtons = document.querySelectorAll(".editor-container .btn-collapse");
+        collapseButtons.forEach(button => button.addEventListener("click", (e) => this._onCollapseClicked(e)));
+    }
+
+    _onExpandClicked(event) {
+        event.currentTarget.parentElement.classList.add("full-screen");
+        this.editor.resize();
+    }
+
+    _onCollapseClicked(event) {
+        event.currentTarget.parentElement.classList.remove("full-screen");
+        this.editor.resize();
+    }
+}
