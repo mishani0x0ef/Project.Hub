285971b78525a8a13f3d4e68111729c21c8ab1bb
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2018-05-14 01:54:33 +0300

    Implement some kind of "Change Password" functionality.
    Add possibility to regenerate password hash for admin account.

--- src/Project.Hub/Controllers/AdminController.cs
+++ src/Project.Hub/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
 ﻿using Common.Cache;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Project.Hub.Config.Interfaces;
+using Project.Hub.Models.Admin;
 using System.Threading.Tasks;
 
 namespace Project.Hub.Controllers
@@ -11,17 +13,25 @@ namespace Project.Hub.Controllers
     {
         private readonly IRawConfigProvider _configProvider;
         private readonly ICache _cache;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IPasswordHasher<IdentityUser> _passwordHasher;
 
-        public AdminController(IRawConfigProvider configProvider, ICache cache)
+        public AdminController(
+            IRawConfigProvider configProvider, 
+            ICache cache,
+            UserManager<IdentityUser> userManager,
+            IPasswordHasher<IdentityUser> passwordHasher)
         {
             _configProvider = configProvider;
             _cache = cache;
+            _userManager = userManager;
+            _passwordHasher = passwordHasher;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string passwordHash = null)
         {
             var config = await _configProvider.GetConfig();
-            return View(config);
+            return View(new RawConfigExtended(config, passwordHash));
         }
 
         [HttpPost]
@@ -36,6 +46,14 @@ namespace Project.Hub.Controllers
         public async Task SaveNlogConfig([FromBody] string config)
         {
             await _configProvider.UpdateNLogConfig(config);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GeneratePasswordHash([FromForm] string password)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var passwordHash = _passwordHasher.HashPassword(user, password);
+            return RedirectToAction(nameof(AdminController.Index), "Admin", new { passwordHash });
         }
     }
 }
