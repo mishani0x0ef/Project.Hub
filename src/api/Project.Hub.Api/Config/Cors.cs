bfe3fbfbf09f4986072576775e7bf9afe9077a75
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2020-02-14 15:51:47 +0200

    #18 configure CORS

---
+++ src/api/Project.Hub.Api/Config/Cors.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Project.Hub.Api.Config
+{
+    internal static class Cors
+    {
+        private const string DevCorsPolicy = "devCorsPolicy";
+
+        /// <summary>
+        /// Add CORS policy with allowed origins to communication with the API.
+        /// <para>(include configurations for development environment as well)</para>
+        /// </summary>
+        public static IServiceCollection AddPreConfiguredCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    DevCorsPolicy,
+                    builder =>
+                    {
+                        var allowedOrigins = new[]
+                        {
+                            "http://localhost:4200",
+                        };
+                        builder
+                            .WithOrigins(allowedOrigins);
+                    });
+            });
+
+            return services;
+        }
+
+        /// <summary>
+        /// Enable CORS policies to configure communication between clients and API.
+        /// </summary>
+        public static IApplicationBuilder UsePreConfiguredCors(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseCors(DevCorsPolicy);
+            }
+
+            return app;
+        }
+    }
+}
--- src/api/Project.Hub.Api/Startup.cs
+++ src/api/Project.Hub.Api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Project.Hub.Api.Config;
 
 namespace Project.Hub.Api
 {
@@ -25,7 +19,9 @@ namespace Project.Hub.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddPreConfiguredCors()
+                .AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,16 +32,15 @@ namespace Project.Hub.Api
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app
+                .UsePreConfiguredCors(env)
+                .UseHttpsRedirection()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
         }
     }
 }
